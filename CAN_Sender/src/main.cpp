1dc33835fe315f2eefaa0027c71d7fc37db3b991
Mezaour Lynda <153217130+lynda-mezaour@users.noreply.github.com>
2024-04-26 23:29:39 +0100

    Add files via upload

---
+++ CAN_Sender/src/main.cpp
@@ -0,0 +1,74 @@
+// Copyright (c) Sandeep Mistry. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+#include <Arduino.h>
+//==================================================================================//
+
+#include <CAN.h>
+
+#define TX_GPIO_NUM   25  // Connects to CTX
+#define RX_GPIO_NUM   26  // Connects to CRX
+//==================================================================================//
+
+void canSender() {
+  // send packet: id is 11 bits, packet can contain up to 8 bytes of data
+  Serial.print ("Sending packet ... ");
+
+  CAN.beginPacket (0x12); 
+ //sets the ID and clears the transmit buffer
+  // CAN.beginExtendedPacket(0xabcdef);
+  CAN.write ('1'); //write data to buffer. data is not sent until endPacket() is called.
+  CAN.write ('2');
+  CAN.write ('3');
+  CAN.write ('4');
+  CAN.write ('5');
+  CAN.write ('6');
+  CAN.write ('7');
+  CAN.write ('8');
+  CAN.endPacket();
+
+  //RTR packet with a requested data length
+  CAN.beginPacket (0x12, 3, true);
+  CAN.endPacket();
+
+  Serial.println ("done");
+
+  delay (1000);
+}
+
+
+//==================================================================================//
+
+void setup() {
+  Serial.begin (115200);
+  while (!Serial);
+  delay (1000);
+
+  Serial.println ("CAN Receiver/Receiver");
+
+  // Set the pins
+  CAN.setPins (RX_GPIO_NUM, TX_GPIO_NUM);
+
+  // start the CAN bus at 500 kbps
+  if (!CAN.begin (500E3)) {
+    Serial.println ("Starting CAN failed!");
+    while (1);
+  }
+  else {
+    Serial.println ("CAN Initialized");
+  }
+}
+
+//==================================================================================//
+
+void loop() {
+  canSender();
+  
+}
+
+//==================================================================================//
+
+
+
+
+
+//==================================================================================//
